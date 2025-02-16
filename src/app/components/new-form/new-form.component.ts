8c14841dac0842f20615b4df63966a175f655e88
ronito19 <ronnie_1019@hotmail.com>
2025-02-16 18:52:56 +0100

    Creacion del campo Url de la imagen

--- src/app/components/new-form/new-form.component.ts
+++ src/app/components/new-form/new-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { INews } from '../../interfaces/inews.interface';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-new-form',
@@ -12,20 +13,7 @@ export class NewFormComponent {
   newNews: INews = { title: "", url: "", texto: "", fecha: new Date(), tipo: ""}
   @Output() catchNew: EventEmitter<INews> = new EventEmitter();
 
-  imagePreview: string | ArrayBuffer | null = null;
-  // Funcion para imagen seleccionada
-  onImageSelected(event: Event): void {
-    const file = (event.target as HTMLInputElement).files?.[0]; // Obtener el archivo seleccionado
-    if (file) {
-      const reader = new FileReader(); // Crear lector de archivos
-      reader.onload = () => {
-        this.imagePreview = reader.result; // Mostrar imagen en vista previa
-        this.newNews.url = reader.result as string; // Guardar imagen
-      };
-      reader.readAsDataURL(file); // Leer el archivo
-    }
-  }
-
+  
   // Funcion para enviar los datos del formulario
   onSubmit(): void {
     console.log(this.newNews)
